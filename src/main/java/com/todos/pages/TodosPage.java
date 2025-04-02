3ecac6ecfebde4fa7d3f35e42d09c7562f5c294d
chaimamosbehi <mosbehi.chayma@gmail.com>
2025-04-02 21:29:08 +0100

    selenium- TestNG

--- src/main/java/com/todos/pages/TodosPage.java
+++ src/main/java/com/todos/pages/TodosPage.java
@@ -1,5 +1,55 @@
 package com.todos.pages;
 
-public class TodosPage {
+import java.io.IOException;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+
+import com.todos.utils.BasePage;
+
+public class TodosPage extends BasePage {
+
+	public TodosPage() throws IOException {
+		
+		PageFactory.initElements(driver, this);
+	
+	}
+	
+	
+	@FindBy(how = How.XPATH, using = "//input[@placeholder='What needs to be done?']")
+	public static WebElement inputField;
+	
+	@FindBy(how = How.XPATH, using = "//label[normalize-space()='Adopter de bonnes pratiques de test']")
+	public static WebElement textToDo;
+	
+	@FindBy(how = How.XPATH, using = "//input[@type='checkbox']")
+	public static WebElement checkboxToDo;
+	
+	public void fillTodo(String todo) {
+		
+		inputField.sendKeys(todo);
+		inputField.sendKeys(Keys.ENTER);
+		
+	}
+	
+	public Boolean isElementDisplayed(WebElement element) {
+		
+		//Boolean isElementDisplayed = element.isDisplayed();
+		//return isElementDisplayed;
+		return element.isDisplayed();
+	}
+	
+	public String checkElementContain(WebElement element) {
+		
+		return element.getText();
+	}
+	
+	public Boolean isChekboxSelected(WebElement element) {
+		
+		return element.isSelected();
+	}
 
 }
--- src/test/java/com/todos/tests/AddTodosTest.java
+++ src/test/java/com/todos/tests/AddTodosTest.java
@@ -1,5 +1,35 @@
 package com.todos.tests;
 
-public class AddTodosTest {
+import java.io.IOException;
 
+import org.junit.jupiter.api.Assertions;
+import org.testng.annotations.Test;
+
+import com.todos.pages.TodosPage;
+import com.todos.utils.Setup;
+
+
+public class AddTodosTest extends Setup {
+
+	public AddTodosTest() throws IOException {
+		super();
+		
+	}
+
+	TodosPage todopage;
+	
+	@Test
+	public void iAddToDo() throws IOException {
+		
+		todopage = new TodosPage();
+		boolean fieldToDo = todopage.isElementDisplayed(TodosPage.inputField);
+		Assertions.assertTrue(fieldToDo);
+		todopage.fillTodo(prop.getProperty("todo1"));
+		
+		String getText = todopage.checkElementContain(TodosPage.textToDo);
+		Assertions.assertTrue(getText.contains(prop.getProperty("todo1")));
+		
+		boolean checkbox = todopage.isChekboxSelected(TodosPage.checkboxToDo);
+		Assertions.assertFalse(checkbox);
+	}
 }
